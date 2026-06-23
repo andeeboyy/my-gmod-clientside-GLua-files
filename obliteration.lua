9384f91a15882ef3033faaffefe8fb7fd72aa75d
andeeboyy <andertu120@gmail.com>
2026-06-22 19:20:17 -0700

    Add files via upload

---
+++ obliteration.lua
@@ -0,0 +1,15 @@
+if timer.Exists("constant_boom") then
+    timer.Remove("constant_boom")
+
+    print("Obliteration Off.")
+
+    else
+
+    print("Obliteration On.")
+
+    timer.Create("constant_boom", 0, 0, function() 
+	LocalPlayer():ConCommand("ent_create prop_physics model models/props_junk/garbage_glassbottle003a.mdl physdamagescale 100000 ExplodeDamage 1000000 ExplodeRadius 1000 modelscale 0.1 massScale 10000; ent_create grenade_ar2")
+    end)
+
+end
+
