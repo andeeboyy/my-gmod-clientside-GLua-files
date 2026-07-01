6b2f17e194c9d38e675b3deb19bcd2cb4895f0ca
andeeboyy <andertu120@gmail.com>
2026-07-01 12:43:36 -0700

    Update obliteration.lua

--- obliteration.lua
+++ obliteration.lua
@@ -1,5 +1,6 @@
-if timer.Exists("constant_boom") then
-    timer.Remove("constant_boom")
+
+if hook.GetTable()["Tick"]["obliterate"] then
+    hook.Remove("Tick", "obliterate")
 
     print("Obliteration Off.")
 
@@ -7,9 +8,8 @@ if timer.Exists("constant_boom") then
 
     print("Obliteration On.")
 
-    timer.Create("constant_boom", 0, 0, function() 
+    hook.Add("Tick", "obliterate", function()
 	LocalPlayer():ConCommand("ent_create prop_physics model models/props_junk/garbage_glassbottle003a.mdl physdamagescale 100000 ExplodeDamage 1000000 ExplodeRadius 500 massScale 10000; ent_create grenade_ar2")
     end)
 
 end
-
